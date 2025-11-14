7bb4d470b430eb1a66d8d7981df5f5297b26d652
Palacioss15 <dapala02@ucm.es>
2025-11-14 15:49:01 +0100

    Update InputManaer.js
    - Ahora funciona mediante suscripcion a eventos que saltan cuando se registra un imput.
    - Ademas, cuando con registro de botones para emitir acciones tras clickear #25

--- Wally like an Egyptian/src/core/InputManager.js
+++ Wally like an Egyptian/src/core/InputManager.js
@@ -1,70 +1,23 @@
-/** Singleton para acceso desde cualquier clase que necesita gestión de Input */
-export default class InputManager {
-  static instance = null;
+import Phaser from "phaser";
 
-  static getInstance(scene) {
-    if (!InputManager.instance) {
-      InputManager.instance = new InputManager(scene);
-    } else {
-      InputManager.instance.scene = scene;
-    }
-    return InputManager.instance;
-  }
+export default class InputManager extends Phaser.Events.EventEmitter {
 
   constructor(scene) {
+    super();
+
     this.scene = scene;
-    this.mouseEnabled = false;
-    this.keyBoardEnabled = false;
-    this.registeredButtons = [];
     this.cursors = null;
     this.keys = {};
+    this.enabled = true;   // puedes activar/desactivar inputs
   }
 
-   /** Habilita el uso de ratón */
-  enableMouse() {
-    this.mouseEnabled = true;
-  }
-
-  /** Deshabilita temporalmente el ratón */
-  disableMouse() {
-    this.mouseEnabled = false;
-  }
-
-  /** Habilita el teclado */
-  enableKeyBoard() {
-    this.keyBoardEnabled = true;
-  }
-
-  /** Deshabilita el teclado */
-  disableKeyBoard() {
-    this.keyBoardEnabled = false;
-  }
-
-  /**
-   * Configura las entradas necesarias para la escena actual.
-   * @param {Object} config - configuración opcional
-   * @example
-   * configureInputs({
-   *   mouse: true,
-   *   keyboard: true,
-   *   keys: ['B', 'ESC'],
-   *   cursors: true
-   * })
-   */
-  configureInputs(config = {}) {
+  configure(config = {}) {
     const input = this.scene.input.keyboard;
 
-    this.mouseEnabled = !!config.mouse;
-    this.keyBoardEnabled = !!config.keyboard;
-
-    // crea cursores si se pide movimiento
     if (config.cursors) {
       this.cursors = input.createCursorKeys();
-    } else {
-      this.cursors = null;
     }
 
-    // crea teclas personalizadas si se piden
     if (config.keys && Array.isArray(config.keys)) {
       this.keys = input.addKeys(
         config.keys.reduce((acc, key) => {
@@ -72,51 +25,43 @@ export default class InputManager {
           return acc;
         }, {})
       );
-    } else {
-      this.keys = {};
     }
   }
 
-  /** Registra un botón UI */
+  // registra un boton para poder ejecutar callbacks
   registerButton(button, callback) {
     button.setInteractive();
-    button.on('pointerdown', () => {
-      if (!this.mouseEnabled) return;
-      this.scene.sound?.play('click');
-      callback();
+
+    button.on("pointerdown", () => {
+        if (!this.enabled) return;
+        this.emit("buttonPressed", button);
+        callback();
     });
+}
 
-    this.registeredButtons.push(button);
-  }
+  // gestión de inputs
+  update() {
+    if (!this.enabled) return;
 
-  /** Detecta ESC y lanza el menú de pausa */
-  handleExit(context) {
-    if (this.keys.ESC && Phaser.Input.Keyboard.JustDown(this.keys.ESC)) {
-      if (context === 'MapScene') {
-        this.scene.scene.launch('PauseMenuGame', this.scene.scene.key);
-        this.scene.scene.pause();
-      } if (context === 'Minigame') {
-        this.scene.scene.launch('PauseMenuMinigame', {
-          parentScene: this.scene.scene.key,
-        });
-        this.scene.scene.pause();
-      } if (context === 'Menu') {
-        this.scene.pause();
-        this.scene.start(this.scene.getParentSceneNode());
+    // 1) Movimiento
+    if (this.cursors) {
+      let x = 0, y = 0;
+
+      if (this.cursors.left.isDown) x = -1;
+      else if (this.cursors.right.isDown) x = 1;
+
+      if (this.cursors.up.isDown) y = -1;
+      else if (this.cursors.down.isDown) y = 1;
+
+      this.emit("move", { x, y });
+    }
+
+    // 2) Teclas individuales definidas en la escena / menu donde se usen
+    for (const keyName in this.keys) {
+      const keyObj = this.keys[keyName];
+      if (Phaser.Input.Keyboard.JustDown(keyObj)) {
+        this.emit("keyDown", keyName);
       }
     }
-  }
-
-  /** Movimiento básico del jugador */
-  getMovementVector() {
-    if (!this.keyBoardEnabled || !this.cursors) return { x: 0, y: 0 };
-
-    let x = 0, y = 0;
-    if (this.cursors.left.isDown) x = -1;
-    else if (this.cursors.right.isDown) x = 1;
-    if (this.cursors.up.isDown) y = -1;
-    else if (this.cursors.down.isDown) y = 1;
-
-    return { x, y };
   }
 }
