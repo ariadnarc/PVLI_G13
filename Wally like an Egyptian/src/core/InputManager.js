77fe06799f9a418e4eced323d7e64827bec9a8ba
Palacioss15 <dapala02@ucm.es>
2025-11-14 01:25:46 +0100

    Gran update InputManager.js
    - Metodo nueva configreInput(), que permite que las clases de tipo Scene decidan que input permiten en ellas.
    - Metodo para registrar callbacks de botones para llamadas a otras Scenes.
    - Metodo que gestiona la tecla 'Esc' en funcion de la Scene en la que te encuentres.  #25

--- Wally like an Egyptian/src/core/InputManager.js
+++ Wally like an Egyptian/src/core/InputManager.js
@@ -1,4 +1,4 @@
-/** Singleton para acceso desde cualquier clase que necesita gestion de Input */
+/** Singleton para acceso desde cualquier clase que necesita gestión de Input */
 export default class InputManager {
   static instance = null;
 
@@ -13,32 +13,14 @@ export default class InputManager {
 
   constructor(scene) {
     this.scene = scene;
-    this.cursors = scene.input.keyboard.createCursorKeys();
-    this.keys = scene.input.keyboard.addKeys({
-      B: Phaser.Input.Keyboard.KeyCodes.B,
-      ESC: Phaser.Input.Keyboard.KeyCodes.ESC,
-    });
     this.mouseEnabled = false;
+    this.keyBoardEnabled = false;
     this.registeredButtons = [];
+    this.cursors = null;
+    this.keys = {};
   }
 
-  /** Registra un botón UI para que use eventos */
-  registerButton(button, callback) {
-    button.setInteractive();
-
-    button.on('pointerover', () => button.setStyle({ backgroundColor: '#bbb' }));
-    button.on('pointerout', () => button.setStyle({ backgroundColor: '#ddd' }));
-
-    button.on('pointerdown', () => {
-      if (!this.mouseEnabled) return;
-      this.scene.sound.play('click');
-      callback();
-    });
-
-    this.registeredButtons.push(button);
-  }
-
-  /** Habilita el uso de ratón en la escena */
+   /** Habilita el uso de ratón */
   enableMouse() {
     this.mouseEnabled = true;
   }
@@ -48,27 +30,90 @@ export default class InputManager {
     this.mouseEnabled = false;
   }
 
-  /** Detecta si se pulsa ESC y lanza el menú correspondiente */
-  handlePause(context) {
-    if (Phaser.Input.Keyboard.JustDown(this.keys.ESC)) {
-      if (context === 'game') {
-        this.scene.scene.launch('PauseMenuGame');
+  /** Habilita el teclado */
+  enableKeyBoard() {
+    this.keyBoardEnabled = true;
+  }
+
+  /** Deshabilita el teclado */
+  disableKeyBoard() {
+    this.keyBoardEnabled = false;
+  }
+
+  /**
+   * Configura las entradas necesarias para la escena actual.
+   * @param {Object} config - configuración opcional
+   * @example
+   * configureInputs({
+   *   mouse: true,
+   *   keyboard: true,
+   *   keys: ['B', 'ESC'],
+   *   cursors: true
+   * })
+   */
+  configureInputs(config = {}) {
+    const input = this.scene.input.keyboard;
+
+    this.mouseEnabled = !!config.mouse;
+    this.keyBoardEnabled = !!config.keyboard;
+
+    // crea cursores si se pide movimiento
+    if (config.cursors) {
+      this.cursors = input.createCursorKeys();
+    } else {
+      this.cursors = null;
+    }
+
+    // crea teclas personalizadas si se piden
+    if (config.keys && Array.isArray(config.keys)) {
+      this.keys = input.addKeys(
+        config.keys.reduce((acc, key) => {
+          acc[key] = Phaser.Input.Keyboard.KeyCodes[key];
+          return acc;
+        }, {})
+      );
+    } else {
+      this.keys = {};
+    }
+  }
+
+  /** Registra un botón UI */
+  registerButton(button, callback) {
+    button.setInteractive();
+    button.on('pointerdown', () => {
+      if (!this.mouseEnabled) return;
+      this.scene.sound?.play('click');
+      callback();
+    });
+
+    this.registeredButtons.push(button);
+  }
+
+  /** Detecta ESC y lanza el menú de pausa */
+  handleExit(context) {
+    if (this.keys.ESC && Phaser.Input.Keyboard.JustDown(this.keys.ESC)) {
+      if (context === 'MapScene') {
+        this.scene.scene.launch('PauseMenuGame', this.scene.scene.key);
         this.scene.scene.pause();
-      } else if (context === 'minigame') {
+      } if (context === 'Minigame') {
         this.scene.scene.launch('PauseMenuMinigame', {
-          minigameScene: this.scene.scene.key,
+          parentScene: this.scene.scene.key,
         });
         this.scene.scene.pause();
+      } if (context === 'Menu') {
+        this.scene.pause();
+        this.scene.start(this.scene.getParentSceneNode());
       }
     }
   }
 
   /** Movimiento básico del jugador */
   getMovementVector() {
+    if (!this.keyBoardEnabled || !this.cursors) return { x: 0, y: 0 };
+
     let x = 0, y = 0;
     if (this.cursors.left.isDown) x = -1;
     else if (this.cursors.right.isDown) x = 1;
-
     if (this.cursors.up.isDown) y = -1;
     else if (this.cursors.down.isDown) y = 1;
 
